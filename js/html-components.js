8d5fa2cb3d7141c1f6cab12d7f7d9720736aef6d
Brodric Young <you22030@byui.edu>
2026-09-21 11:32:37 -0600

    header links

--- js/html-components.js
+++ js/html-components.js
@@ -4,16 +4,16 @@ async function loadComponent(id, file) {
     document.getElementById(id).innerHTML = html;
 }
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//     await loadComponent("header", "../html-components/header.html");
-//     await loadComponent("footer", "../html-components/footer.html");
-// });
-
 document.addEventListener("DOMContentLoaded", async () => {
-    // Check if the current path is the root index page
-    const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("/index.html");
-    const pathPrefix = isIndex ? "./html-components/" : "../html-components/";
-
-    await loadComponent("header", `${pathPrefix}header.html`);
-    await loadComponent("footer", `${pathPrefix}footer.html`);
+    await loadComponent("header", "../html-components/header.html");
+    await loadComponent("footer", "../html-components/footer.html");
 });
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     // Check if the current path is the root index page
+//     const isIndex = window.location.pathname === "/" || window.location.pathname.endsWith("/index.html");
+//     const pathPrefix = isIndex ? "./html-components/" : "../html-components/";
+
+//     await loadComponent("header", `${pathPrefix}header.html`);
+//     await loadComponent("footer", `${pathPrefix}footer.html`);
+// });
